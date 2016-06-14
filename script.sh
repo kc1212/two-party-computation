f6849f37eb29d2f264418391c7cb1dcacd28cda6
kc1212 <kc1212@users.noreply.github.com>
2016-06-14 21:11:18 +0200

    Script for running the program many times

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -x
+
+res=results.txt
+echo -n "" > "$res"
+for i in `seq 5 5 30`; do
+    for j in `seq 5`; do # repeat 5 times to average
+        echo -n "$i," >> "$res"
+        java com.twopc.Main $i | head -n 4 | sed 's/^.*:\s*//g' | paste -d, - - - - >> "$res"
+    done
+done
