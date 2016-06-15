70dc0437857d932847641144a4c5a67c3f2d4ccf
kc1212 <kc1212@users.noreply.github.com>
2016-06-15 23:51:06 +0200

    Put results in different files

--- script.sh
+++ script.sh
@@ -4,10 +4,9 @@ set -e
 set -u
 set -x
 
-res="results.txt"
-echo -n "" > "$res"
-
 for i in $(seq 5 5 30); do
+    res="results$i.csv"
+    echo -n "" > "$res"
     for j in $(seq 5); do # repeat 5 times to average
         echo -n "$i," >> "$res"
         java -classpath out/production/two-party-computation com.twopc.Main "$i" 40 | head -n 4 | sed 's/^.*:\s*//g' | paste -d, - - - - >> "$res"
